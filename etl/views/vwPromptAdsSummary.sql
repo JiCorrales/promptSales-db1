c83814a3dbd3c7f47bbab3000d67fc70aab3705d
Andres Rodríguez <andres.rodz.aco@gmail.com>
2025-11-29 15:33:10 -0600

    Fix PromptAds View

--- etl/views/vwPromptAdsSummary.sql
+++ etl/views/vwPromptAdsSummary.sql
@@ -1,4 +1,3 @@
-
 CREATE OR ALTER VIEW dbo.vwPromptAdsSummary
 AS
 WITH MetricsByAd AS (
@@ -32,18 +31,8 @@ MarketsByAd AS (
     JOIN dbo.TargetAudience ta ON ta.TargetAudienceId = aa.TargetAudienceId
     WHERE aa.enabled = 1
     GROUP BY aa.AdId
-),
-CurrencyByAd AS (
-    SELECT
-        ct.AdId,
-        STRING_AGG(DISTINCT cur.isoCode, ', ') AS CurrencyIsoCodes,
-        STRING_AGG(DISTINCT cur.name, ', ')    AS CurrencyNames
-    FROM dbo.CampaignTransactions ct
-    JOIN dbo.Payments    pay ON pay.PaymentId   = ct.PaymentId
-    JOIN dbo.Currencies  cur ON cur.CurrencyId  = pay.CurrencyId
-    GROUP BY ct.AdId
 )
-SELECT
+SELECT TOP 100 
     camp.CampaignId,
     camp.name        AS CampaignName, 
     camp.startDate,
@@ -59,15 +48,10 @@ SELECT
     ad.enabled       AS AdEnabled,
     ast.name         AS AdStatus,
     at.name          AS AdType,
-    ctry.name       AS Country,
-    crrcy.name      AS Currency,
+
 
     COALESCE(chs.Channels,      'Sin canal')   AS Channels,
     COALESCE(mks.TargetMarkets, 'Sin mercado') AS TargetMarkets,
-    COALESCE(cur.CurrencyIsoCodes, 'Sin moneda') AS CurrencyCodes,
-    COALESCE(cur.CurrencyNames,    'Sin moneda') AS CurrencyNames,
-    COALESCE(cc.CountryName,       'Sin país')   AS CompanyCountry,
-    cc.CountryId                                   AS CompanyCountryId,
 
     ISNULL(m.TotalImpressions,  0) AS TotalImpressions,
     ISNULL(m.TotalClicks,       0) AS TotalClicks,
@@ -81,22 +65,7 @@ JOIN dbo.Companies comp    ON comp.CompanyId   = camp.CompanyId
 JOIN dbo.Ads ad            ON ad.CampaignId    = camp.CampaignId
 JOIN dbo.AdStatus ast      ON ast.AdStatusId   = ad.AdStatusId
 JOIN dbo.AdTypes  at       ON at.AdTypeId      = ad.AdTypeId
-JOIN dbo.Countries ctry    ON ctry.CountryId   = ad.CountryId
-JOIN dbo.Currencies crrcy   ON crrcy.CurrencyId = ad.CurrencyId
-LEFT JOIN MetricsByAd  m   ON m.AdId           = ad.AdId
+right JOIN MetricsByAd  m   ON m.AdId           = ad.AdId
 LEFT JOIN ChannelsByAd chs ON chs.AdId         = ad.AdId
 LEFT JOIN MarketsByAd  mks ON mks.AdId         = ad.AdId
-LEFT JOIN CurrencyByAd cur ON cur.AdId         = ad.AdId
-OUTER APPLY (
-    SELECT TOP 1
-        ctr.CountryId,
-        ctr.name AS CountryName
-    FROM dbo.CompanyAddresses ca
-    JOIN dbo.Addresses addr ON addr.AddressId = ca.AddressId
-    JOIN dbo.Cities    ci   ON ci.CityId      = addr.CityId
-    JOIN dbo.States    st   ON st.StateId     = ci.StateId
-    JOIN dbo.Countries ctr  ON ctr.CountryId  = st.CountryId
-    WHERE ca.CompanyId = comp.CompanyId
-    ORDER BY ca.isPrimary DESC, ca.CompanyAddressId
-) cc
 WHERE ISNULL(ad.processed, '') <> 'procesado';
